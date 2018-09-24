98091a1d83fe6ce7c2d60e0549b293b15cafba07
Harry Hogg <harry@hogg.io>
2018-09-24 16:04:23 +0100

    feat(Button): Added 'active' and 'style' props

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -6,20 +6,27 @@ import './Button.css';
 
 export default class Button extends Component {
   static propTypes = {
+    /** Retains the Button in its active state */
+    active: PropTypes.bool,
     /** Colours that is applied to the button to indicate the type of action */
     color: PropTypes.oneOf(['accent', 'negative', 'positive']),
     /** A styled disabled state that disables all interactions */
     disabled: PropTypes.bool,
+    /** Visual style of the Button */
+    style: PropTypes.oneOf(['fill', 'outline']),
   };
 
   static defaultProps = {
     color: 'accent',
+    style: 'outline',
   };
 
   render() {
-    const { color, ...rest } = this.props;
+    const { active, color, style, ...rest } = this.props;
     const classes = classnames('Button', {
+      'Button--active': active,
       [`Button--${color}`]: color,
+      [`Button--${style}`]: style,
     });
 
     return (
