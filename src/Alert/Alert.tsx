293a32d58c751e669be4e36c5f5ef202e966f49f
Harry Hogg <harry@hogg.io>
2019-12-24 21:55:46 +0000

    fix(Alert): Renamed prop 'style' to 'type'
    BREAKING CHANGE: Renamed Alerts  prop 'style' to 'type'.

--- src/Alert/Alert.tsx
+++ src/Alert/Alert.tsx
@@ -3,25 +3,24 @@ import classnames from 'classnames';
 import Base, { Props as BaseProps } from '../Base/Base';
 import './Alert.css';
 
-interface Props extends BaseProps {
-  /** Colours that is applied to the Alert to indicate the type of action */
+export interface Props extends BaseProps {
+  /** Colour that is applied to the Alert to indicate the type of action */
   color: 'accent' | 'negative' | 'positive';
   /** Whether the alert is flashing, a great way to give it extra attention. */
   flash?: boolean;
   /** Style of how the alert is presented */
-  style: 'outline' | 'solid';
+  type: 'outline' | 'solid';
 }
 
-
 const Alert: React.FunctionComponent<Props> = (props: Props) => {
-  const { color, flash, style, ...rest } = props;
-  const classes = classnames('Alert', `Alert--${color}`, `Alert--${style}`, {
+  const { color, flash, type, ...rest } = props;
+  const classes = classnames('Alert', `Alert--${color}`, `Alert--${type}`, {
     'Alert--flash': flash,
   });
 
   return (
     <Base { ...rest }
-        borderSize={ style === 'outline' ? 'x2' : undefined }
+        borderSize={ type === 'outline' ? 'x2' : undefined }
         className={ classes } />
   );
 };
