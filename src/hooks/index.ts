4d5517aa463b3fb53249e8263eeb0e9b5d60baf2
Harry Hogg <harry@hogg.io>
2020-01-24 18:16:04 +0000

    feat(useMatchMedia): Added MathMedia API hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,6 @@
 export { default as useEventListener } from './useEventListener';
-export { default as useIsInView } from './useIsInView';
-export { default as useMediaQuery } from './useMediaQuery';
+export { default as useIntersectionObserver } from './useIntersectionObserver';
+export { default as useMatchMedia } from './useMatchMedia';
 export { default as useResizeObserver } from './useResizeObserver';
 export { default as useTheme } from './useTheme';
 export { default as useWindowScrollTo } from './useWindowScrollTo';
