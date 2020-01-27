11a87d2ad213b7870a6de2c2ec0ceefc4f2458ce
Harry Hogg <harry@hogg.io>
2020-01-27 16:32:36 +0000

    fix(Table): Clickable styling for other interaction events.
    Uses clickable prop to apply interaction styling, to support other interaction events.

--- src/Table/TableRow.tsx
+++ src/Table/TableRow.tsx
@@ -5,13 +5,15 @@ import Base, { Attributes, BaseProps } from '../Base/Base';
 export interface TableRowProps extends BaseProps {}
 
 const TableRow: React.RefForwardingComponent<HTMLTableRowElement, Attributes<HTMLTableRowElement, TableRowProps>> = (props, ref) => {
+  const { clickable, ...rest } = props;
   const classes = classnames('Table__row', {
-    'Table__row--clickable': props.onClick,
+    'Table__row--clickable': clickable,
   });
 
   return (
-    <Base { ...props }
+    <Base { ...rest }
         className={ classes }
+        clickable={ clickable }
         ref={ ref }
         tag="tr" />
   );
