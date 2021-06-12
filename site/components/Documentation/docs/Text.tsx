bd887c95e2fb3e3ecf86398fea3e15858cba7718
Harry Hogg <harry@hogg.io>
2021-06-13 00:36:58 +0100

    fix(Text): Improved sizing and spacings

--- site/components/Documentation/docs/Text.tsx
+++ site/components/Documentation/docs/Text.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react' ;
-import { Text, TextProps } from 'preshape';
+import { Box, Text, TextProps } from 'preshape';
 import Showcase from '../Showcase';
 import { CatalogueItem } from '.';
 
@@ -13,21 +13,36 @@ const Item: CatalogueItem<{
   showcase: {
     Component: (props) => (
       <Showcase>
-        <Text heading size="x5" { ...props.Text } margin="x3">
-          The spectacle before us was indeed sublime.
-        </Text>
+        <Box
+            backgroundColor="background-shade-1"
+            padding="x6"
+            textColor="text-shade-1"
+            theme="night">
+          <Box maxWidth="500px">
+            <Text heading margin="x3" size="x4">The spectacle before us was indeed sublime.</Text>
+            <Text heading margin="x3" size="x3">Apparently we had reached a great height in the atmosphere,
+              for the sky was a dead black, and the stars had ceased
+              to twinkle.</Text>
+          </Box>
+        </Box>
 
-        <Text>
-          Apparently we had reached a great height in the atmosphere,
-          for the sky was a dead black, and the stars had ceased
-          to twinkle. By the same illusion which lifts the horizon
-          of the sea to the level of the spectator on a hillside,
-          the sable cloud beneath was dished out, and the car seemed
-          to float in the middle of an immense dark sphere, whose
-          upper half was strewn with silver. Looking down into the
-          dark gulf below, I could see a ruddy light streaming
-          through a rift in the clouds.
-        </Text>
+        <Box margin="x6" maxWidth="500px">
+          <Text { ...props.Text } margin="x3">
+            Apparently we had reached a great height in the atmosphere,
+            for the sky was a dead black, and the stars had ceased
+            to twinkle. By the same illusion which lifts the horizon
+            of the sea to the level of the spectator on a hillside,
+            the sable cloud beneath was dished out, and the car seemed
+            to float in the middle of an immense dark sphere, whose
+            upper half was strewn with silver. Looking down into the
+            dark gulf below, I could see a ruddy light streaming
+            through a rift in the clouds.
+          </Text>
+
+          <Text margin="x3" size="x1" strong>Apparently we had reached a great height in the atmosphere,
+            for the sky was a dead black, and the stars had ceased
+            to twinkle.</Text>
+        </Box>
       </Showcase>
     ),
     state: {
