a14133ef53ed047ea79ce5877168165459243dd0
Harry Hogg <harry@hogg.io>
2023-05-14 17:09:45 +0100

    feat(Modal): Added ModalPadding component to access modal paddings

---
+++ workspaces/package/src/Modal/ModalPadding.tsx
@@ -0,0 +1,59 @@
+import React, { forwardRef } from 'react';
+import Box, { BoxProps } from '../Box/Box';
+import { useModalContext } from './Modal';
+
+/**
+ * A component apply paddings of a Modal.
+ */
+export interface ModalPaddingProps extends BoxProps {
+  /**
+   * Whether or not to apply padding to the top of the Modal.
+   *
+   * @default true
+   */
+  top?: boolean;
+  /**
+   * Whether or not to apply padding to the bottom of the Modal.
+   *
+   * @default true
+   */
+  bottom?: boolean;
+  /**
+   * Whether or not to apply padding to the left of the Modal.
+   *
+   * @default true
+   */
+  left?: boolean;
+  /**
+   * Whether or not to apply padding to the right of the Modal.
+   *
+   * @default true
+   */
+  right?: boolean;
+}
+
+const ModalPadding: React.ForwardRefRenderFunction<any, ModalPaddingProps> = (
+  props,
+  ref
+) => {
+  const {
+    top = true,
+    bottom = true,
+    left = true,
+    right = true,
+    ...rest
+  } = props;
+  const { paddingHorizontal, paddingVertical } = useModalContext();
+
+  return (
+    <Box
+      {...rest}
+      paddingLeft={left ? paddingHorizontal : undefined}
+      paddingRight={right ? paddingHorizontal : undefined}
+      paddingTop={top ? paddingVertical : undefined}
+      paddingBottom={bottom ? paddingVertical : undefined}
+    />
+  );
+};
+
+export default forwardRef(ModalPadding);
