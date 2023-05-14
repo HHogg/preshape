39868962696d12ec9cadea0c694e12f2c1adac8a
Harry Hogg <harry@hogg.io>
2023-05-14 17:34:28 +0100

    feat(ESM): Updated Preshape to be built using vite and output ESM and UMD builds. CSS is no longer imported by default and provided as a style.css file in the dist folder to be imported separately.

---
+++ workspaces/package/vite.config.ts
@@ -0,0 +1,33 @@
+import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'preshape',
+      formats: ['es', 'umd'],
+      fileName: (format) => `preshape.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-router-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react-router-dom': 'ReactRouterDOM',
+        },
+      },
+    },
+  },
+}));
