0e588de9041a95c21f7ec17c2d585eee6d01da42
Harry Hogg <harry@hogg.io>
2023-05-14 17:14:19 +0100

    feat(Spinner): Added Spinner component

---
+++ workspaces/package/src/Spinner/Spinner.tsx
@@ -0,0 +1,34 @@
+import { MotionProps } from 'framer-motion';
+import * as Icons from '../Icon';
+import Motion from '../Motion/Motion';
+import React, { forwardRef } from 'react';
+
+/**
+ * A spinner component that can be used to indicate loading.
+ */
+export interface SpinnerProps extends MotionProps {
+  /** Size of the spinner */
+  size?: Icons.Props['size'];
+}
+
+const Spinner: React.ForwardRefRenderFunction<SVGSVGElement, SpinnerProps> = (
+  { size = '1.5rem', ...rest },
+  ref
+) => {
+  return (
+    <Motion
+      {...rest}
+      animate={{ rotate: 360 }}
+      display="block"
+      transition={{
+        ease: 'linear',
+        repeat: Infinity,
+        duration: 2,
+      }}
+    >
+      <Icons.Loader display="block" ref={ref} size={size} />
+    </Motion>
+  );
+};
+
+export default forwardRef(Spinner);
