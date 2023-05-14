60c932b9cd71de9d7f623a72bd120e9368ec9a84
Harry Hogg <harry@hogg.io>
2023-05-14 17:17:11 +0100

    fix(useResizeObserver): Change size type
    BREAKING CHANGE: useResizeObserver now returns a xy object instead of a DOMRect

--- workspaces/package/src/hooks/useResizeObserver.ts
+++ workspaces/package/src/hooks/useResizeObserver.ts
@@ -1,18 +1,22 @@
 import { useEffect, useState, Ref } from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 
-export default <T extends Element = Element>(): [
-  DOMRectReadOnly,
-  Ref<T>,
-  T | null
-] => {
+type Size = {
+  width: number;
+  height: number;
+};
+
+export default <T extends Element = Element>(): [Size, Ref<T>, T | null] => {
   const [node, setNode] = useState<T | null>(null);
-  const [size, setSize] = useState<DOMRectReadOnly>(new DOMRect());
+  const [size, setSize] = useState<Size>({ width: 0, height: 0 });
 
   useEffect(() => {
     const observer = new ResizeObserver((entries) => {
       if (entries.length) {
-        setSize(entries[0].contentRect);
+        setSize({
+          width: entries[0].contentRect.width,
+          height: entries[0].contentRect.height,
+        });
       }
     });
 
