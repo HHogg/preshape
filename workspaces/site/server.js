908e45f6584799390c9e43641888f0d73ed13fe6
Harry Hogg <harry@hogg.io>
2023-06-23 17:08:40 +0100

    chore(vite): Fixed linting in scripts

--- workspaces/site/server.js
+++ workspaces/site/server.js
@@ -9,7 +9,7 @@ const app = express();
 export async function createServer() {
   const resolve = (p) => path.resolve(__dirname, p);
 
-  let vite = null;
+  const vite = null;
 
   app.use((await import('compression')).default());
   app.use(
@@ -27,10 +27,10 @@ export async function createServer() {
         'utf-8'
       );
       const render = (await import('./dist/server/entry-server.js')).SSRRender;
-      const appHtml = render(url); //Rendering component without any client side logic de-hydrated like a dry sponge
-      const html = template.replace(`<!--app-html-->`, appHtml); //Replacing placeholder with SSR rendered components
+      const appHtml = render(url); // Rendering component without any client side logic de-hydrated like a dry sponge
+      const html = template.replace(`<!--app-html-->`, appHtml); // Replacing placeholder with SSR rendered components
 
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html); //Outputing final html
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html); // Outputting final html
     } catch (e) {
       console.error(e);
       res.status(500).end(e.toString());
@@ -42,6 +42,7 @@ export async function createServer() {
 
 createServer().then(({ app }) =>
   app.listen(3033, () => {
+    // eslint-disable-next-line no-console
     console.log('http://localhost:3033');
   })
 );
