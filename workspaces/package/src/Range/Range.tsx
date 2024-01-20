09f3d108cc8fd83661ca374c3e77a33959cd6925
Harry Hogg <harry@hogg.io>
2024-01-20 11:33:45 +0000

    fix(Range): Pass through min, max and step to input element

--- workspaces/package/src/Range/Range.tsx
+++ workspaces/package/src/Range/Range.tsx
@@ -17,12 +17,15 @@ export const Range = forwardRef<HTMLInputElement, RangeProps>((props, ref) => {
     disabled,
     gap = 'x3',
     invalid,
+    max,
+    min,
     name,
     onChange,
     padding,
     paddingHorizontal = 'x6',
     paddingVertical = 'x2',
     readOnly,
+    step,
     value,
     ...rest
   } = props;
@@ -49,11 +52,14 @@ export const Range = forwardRef<HTMLInputElement, RangeProps>((props, ref) => {
         disabled={disabled}
         grow
         onChange={onChange}
+        max={max}
+        min={min}
         name={name}
         paddingHorizontal={padding || paddingHorizontal}
         paddingVertical={padding || paddingVertical}
         readOnly={readOnly}
         ref={ref}
+        step={step}
         tag="input"
         type="range"
         value={value}
