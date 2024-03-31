2b2f8b667c4411ec52494999648c208259df348b
Harry Hogg <harry@hogg.io>
2024-03-31 22:55:38 +0100

    fix(Article): Allow paragraph tag to be customised

--- workspaces/package/src/Article/ArticleParagraph.tsx
+++ workspaces/package/src/Article/ArticleParagraph.tsx
@@ -7,5 +7,5 @@ export const ArticleParagraph = forwardRef<
   HTMLDivElement,
   ArticleParagraphProps
 >((props, ref) => {
-  return <Text lineHeight={1.8} margin="x3" {...props} ref={ref} tag="p" />;
+  return <Text lineHeight={1.8} margin="x3" tag="p" {...props} ref={ref} />;
 });
