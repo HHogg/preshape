739ecfcd9642f349556b6726d24775b4ad8f192b
Harry Hogg <harry@hogg.io>
2024-10-05 08:52:48 +0100

    chore(dev): Run prettier on pictograms after generation

--- workspaces/package/vite.config.ts
+++ workspaces/package/vite.config.ts
@@ -5,10 +5,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [
-    react(),
-    dts(),
-  ],
+  plugins: [react(), dts()],
   build: {
     sourcemap: true,
     lib: {
